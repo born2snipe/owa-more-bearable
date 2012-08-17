9e133279ec9f1e6f94dc81bc5a5c0494e110da3f
Dan Dudley <born2snipe@gmail.com>
2012-08-16 19:54:58 -0500

    added hotkey for checking messages

--- javascripts/bearable.js
+++ javascripts/bearable.js
@@ -3,11 +3,15 @@
 	var totalNumberOfRows = 0;
 	var currentRowHighlighted = 0;
 
+	generateEmailId = function(index) {
+		return "__email_id__"+index;
+	}
+
 	assignIdsToEmails = function() {
 		var count = 0;
 		
 		$(".lvw tr").each(function() {
-			$(this).attr("id",  "__email_id__"+count++);
+			$(this).attr("id",  generateEmailId(count++));
 		});
 		totalNumberOfRows = count;
 	};
@@ -17,7 +21,7 @@
 	};
 
 	hightlightRow = function(index) {
-		var row = $("#__email_id__"+index);
+		var row = $("#"+generateEmailId(index));
 		row.addClass("sl");
 		row[0].scrollIntoView();
 		row.find('input').focus();
@@ -25,7 +29,7 @@
 	};
 
 	unhightlightRow = function(index) {
-		$("#__email_id__"+index).removeClass("sl");
+		$("#"+generateEmailId(index)).removeClass("sl");
 	};
 
 	moveDownRow = function() {
@@ -50,11 +54,16 @@
 		$("#lnkHdrdelete").click();
 	};
 
+	checkMessages = function() {
+		$("#lnkHdrcheckmessages").click();
+	}
+
 	bindKeys = function() {
 		$(document).bind('keydown', 'j', moveDownRow);
 		$(document).bind('keydown', 'k', moveUpRow);
 		$(document).bind('keydown', 'm', clickMove);
 		$(document).bind('keydown', 'd', clickDelete);
+		$(document).bind('keydown', 'r', checkMessages);
 	};
 
 	assignIdsToEmails();
